fe9ab941f6fa31ae9554b8193bf8f3a14ced106d
Polina G <dippolina@yandex.ru>
2021-12-22 17:06:21 +0300

    task

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,21 +33,28 @@
 //    }
 // }
 
+
+
+
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {};
+    data.client = name + ' ' + phone;
+    data.order = {};
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    data.order.sum = sum;
+    data.goods = [];
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1){
+        newArray = {
+            "title": goods[i].title,
+            "count": goods[i].count
+        }
+        data.goods.push(newArray);
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
+
+
